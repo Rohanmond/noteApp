06a63397391e8cb20826264f598235ca4be57332
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-10 19:54:28 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { NotesProvider } from "./contexts/notes-context";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <NotesProvider>
+      <App />
+    </NotesProvider>
+  </StrictMode>,
+  rootElement
+);
